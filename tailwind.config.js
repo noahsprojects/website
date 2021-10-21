806b75bda2fae03457640fde56a8568887c1fd1f
Noah Knegt <github@noahknegt.com>
2021-10-21 18:03:59 +0200

    core: Let the website look like discord
    Signed-off-by: Noah Knegt <github@noahknegt.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // class, 'media' or boolean
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          900: '#202225',
+          800: '#2f3136',
+          700: '#36393f',
+          600: '#4f545c',
+          400: '#d4d7dc',
+          300: '#e3e5e8',
+          200: '#ebedef',
+          100: '#f2f3f5',
+        },
+      },
+      spacing: {
+        88: '22rem',
+      },
+    },
   },
   variants: {
     extend: {},
